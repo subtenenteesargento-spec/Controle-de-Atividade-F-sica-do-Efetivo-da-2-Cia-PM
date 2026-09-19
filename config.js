95e88d8f30b291e94809b432f8ea0902bafb8dab
subtenenteesargento-spec <subtenenteesargento@gmail.com>
2026-09-19 15:01:41 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://nnuiglrkzfqoenayabuh.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_URyacmjxd1LFv4rbpnKXaQ_kvizV01A",
+  APP_NAME: "Controle de Atividade Física do Efetivo"
+};
